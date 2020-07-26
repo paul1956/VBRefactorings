a124c1d436a05b770e7242ddbc67340ee98f5c1c
Paul M Cohen <paul1956@users.noreply.github.com>
2020-07-26 01:10:08 -0700

    Replace VSIX with one that works

--- AddAsClause.VSIX/My Project/AssemblyInfo.vb
+++ AddAsClause.VSIX/My Project/AssemblyInfo.vb
@@ -1,22 +1,20 @@
-﻿' Licensed to the .NET Foundation under one or more agreements.
-' The .NET Foundation licenses this file to you under the MIT license.
-' See the LICENSE file in the project root for more information.
+﻿Imports Microsoft.VisualBasic
 
 Imports System.Reflection
-Imports System.Resources
+Imports System.Runtime.CompilerServices
 Imports System.Runtime.InteropServices
 
 ' General Information about an assembly is controlled through the following 
 ' set of attributes. Change these attribute values to modify the information
 ' associated with an assembly.
-<Assembly: AssemblyTitle("Visual Basic Refactoring")>
+<Assembly: AssemblyTitle("AddAsClause.VSIX")>
 <Assembly: AssemblyDescription("")>
 <Assembly: AssemblyConfiguration("")>
-<Assembly: AssemblyCompany("Travel py Paul")>
-<Assembly: AssemblyProduct("VBRefactorings")>
-<Assembly: AssemblyCopyright("Copyright (c) .NET Foundation and Contributors")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("AddAsClause.VSIX")>
+<Assembly: AssemblyCopyright("")>
 <Assembly: AssemblyTrademark("")>
-<Assembly: NeutralResourcesLanguage("en-US")>
+<Assembly: AssemblyCulture("")>
 
 ' Setting ComVisible to false makes the types in this assembly not visible 
 ' to COM components.  If you need to access a type in this assembly from 
